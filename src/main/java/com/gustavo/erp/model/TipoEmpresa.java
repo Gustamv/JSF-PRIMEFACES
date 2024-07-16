bdafd823f30c940f43165dbe4ef6f137154347bb
Gustavo Melo <55670273+Gustamv@users.noreply.github.com>
2024-07-16 20:56:29 -0300

    Arquivos inciais

---
+++ src/main/java/com/gustavo/erp/model/TipoEmpresa.java
@@ -0,0 +1,19 @@
+package com.gustavo.erp.model;
+
+public enum TipoEmpresa {
+	
+	MEI("Microempreendedor Individual"),
+	EIRELI("Empresa INdividual de Responsabilidade Limitada"),
+	LTDA("Sociedade Limitada"),
+	SA("Sociedade Anônima");
+	
+	private String descricao;
+	
+	TipoEmpresa(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+}
